2f67704b0adfc319017f9c53feb081e12dce77d8
Tibojkt <91432949+Tibojkt@users.noreply.github.com>
2022-10-07 13:22:38 +0800

    test

--- utils/baseApiUrl.js
+++ utils/baseApiUrl.js
@@ -1,6 +1,6 @@
 const baseApiUrl =
   process.env.NODE_ENV === "production"
-    ? "residenceouleta-o9wsivlgf-tibojkt.vercel.app"
+    ? "http://localhost:1337"
     : "http://localhost:1337";
 
 export default baseApiUrl;
