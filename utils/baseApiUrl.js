4cbc2665664b432886cb932dc618a010d2624937
Tibojkt <91432949+Tibojkt@users.noreply.github.com>
2022-10-07 13:11:28 +0800

    change localhost

--- utils/baseApiUrl.js
+++ utils/baseApiUrl.js
@@ -1,5 +1,6 @@
-const baseApiUrl = process.env.NODE_ENV === "production" 
-? 'http://localhost:1337' 
-: 'http://localhost:1337';
+const baseApiUrl =
+  process.env.NODE_ENV === "production"
+    ? "http://localhost:1437"
+    : "http://localhost:1437";
 
 export default baseApiUrl;
