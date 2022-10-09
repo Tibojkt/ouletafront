e8c93a0c6f665dc5be5b8427a32c8ee4751732de
Tibojkt <91432949+Tibojkt@users.noreply.github.com>
2022-10-09 14:22:58 +0800

    add herokuapp

--- utils/baseApiUrl.js
+++ utils/baseApiUrl.js
@@ -1,6 +1,6 @@
 const baseApiUrl =
   process.env.NODE_ENV === "production"
-    ? "http://localhost:1337"
+    ? "https://ouleta.herokuapp.com"
     : "http://localhost:1337";
 
 export default baseApiUrl;
